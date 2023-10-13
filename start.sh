8670930cdab840fcb88c6024bcec1d8c0df327e8
KVOFJOFH <141811117+KVOFJOFH@users.noreply.github.com>
2023-10-13 18:04:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/shahidalamkkbk/Auto-search-tamil-bot /Auto-search-tamil-bot
+  git clone https://github.com/KVOFJOFH/Shahrukhkhan /Shahrukhkhan
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /Shahrukhkhan
 fi
-cd /Auto-search-tamil-bot
+cd /Shahrukhkhan
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
